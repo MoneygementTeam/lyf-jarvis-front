e474d4d4faae40453f94b5c53436b7c48e3752d7
sinseongsu <tlstjdtn321@naver.com>
2024-10-27 02:14:27 +0900

    feat: 기본 페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import MapComponent from '@/components/mapbox/MapBox'
+import { Content } from '@/components/canvas/Content'
+import Modal from '@/components/modal/modal/Modal'
+import { useModalStore } from '@/store/useModalStore'
+import styles from '@/styles/Home.module.scss'
+import { useDijkstraStore } from '@/store/useDijkstraStore'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { isShow, setIsShow } = useModalStore()
+  const { setIsSimulation } = useDijkstraStore()
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  return (
+    <div id="bodyWrapper">
+      <Modal
+        isVisible={isShow}
+        heading=""
+        onClickCloseBtn={() => setIsShow(false)}
+      >
+        <p className={styles.text}>Follow the travel route.</p>
+
+        <button
+          type="button"
+          onClick={() => {
+            setIsShow(false)
+            setIsSimulation(true)
+          }}
+          className={styles.confirmBtn}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          OK
+        </button>
+      </Modal>
+      <MapComponent />
+      <Content />
     </div>
-  );
+  )
 }
